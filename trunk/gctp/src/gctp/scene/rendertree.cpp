bb6b9d9ed07b8219c4ff2bc5f3ced1b204fb6e5e
osamu.takasugi <osamu.takasugi@fdeeef8c-811c-0410-9bc5-e1f93fa3fba2>
2008-05-11 23:15:39 +0000

    とりあえずレンダーツリーという用語のほうが一般的みたいなので名前変更

--- trunk/gctp/src/gctp/scene/rendertree.cpp
+++ trunk/gctp/src/gctp/scene/rendertree.cpp
@@ -1,18 +1,18 @@
 /** @file
- * GameCatapult レンダリングツリークラス
+ * GameCatapult レンダーツリークラス
  *
  * @author SAM (T&GG, Org.)<sowwa_NO_SPAM_THANKS@water.sannet.ne.jp>
  * @date 2004/01/29 21:36:50
  * Copyright (C) 2001,2002,2003,2004 SAM (T&GG, Org.). All rights reserved.
  */
 #include "common.h"
-#include <gctp/scene/renderingtree.hpp>
+#include <gctp/scene/rendertree.hpp>
 
 using namespace std;
 
 namespace gctp { namespace scene {
 
-	GCTP_IMPLEMENT_CLASS_NS(gctp, RenderingTree, RenderingNode);
+	GCTP_IMPLEMENT_CLASS_NS(gctp, RenderTree, Object);
 
 	/** 指定の階層ツリーの複製を、子ノードに連結。
 	 *
@@ -20,22 +20,28 @@ namespace gctp { namespace scene {
 	 * @date 2004/02/12 5:19:18
 	 * Copyright (C) 2001,2002,2003,2004 SAM (T&GG, Org.). All rights reserved.
 	 */
-	void RenderingTree::merge(const RenderingTree &src)
+	void RenderTree::merge(const RenderTree &src)
 	{
 		get()->push(src->dup());
 	}
 
 	namespace {
-		bool renderingVisitor(const RenderingTree::NodeType &n)
-		{
-			if(n.val->onEnter()) n.visitChildrenConst(renderingVisitor);
-			return n.val->onLeave();
-		}
+		class RenderVisitor {
+		public:
+			RenderVisitor(float delta) : delta_(delta) {}
+			bool operator()(const RenderTree::NodeType &n)
+			{
+				if(n.val->onEnter(delta_)) n.visitChildrenConst(*this);
+				return n.val->onLeave(delta_);
+			}
+		private:
+			float delta_;
+		};
 	}
 
-	void RenderingTree::draw() const
+	void RenderTree::draw(float delta) const
 	{
-		visit(renderingVisitor);
+		visit(RenderVisitor(delta));
 	}
 
 }} // namespace gctp::scene
